a81332ca2136319c1d46dc6577d28e0c27215b44
Alex Turcu <turcu.alex5@gmail.com>
2017-09-15 17:16:03 +0300

    ss

--- src/js/carousel.js
+++
@@ -1,5 +0,0 @@
-$('.row-equal').slick({
-  slidesToShow: 5,
-  nextArrow: $(".nextButton"),
-  prevArrow: $(".prevButton")
-});
--- src/js/slickInit.js
+++ src/js/slickInit.js
@@ -1,11 +1,46 @@
 $(document).ready(function(){
+  // First Carousel init
       $('.carousel').slick({
-
         dots: true,
         speed: 500,
         lazyLoad: 'progressive',
         arrows: true,
-        
 
       });
-    });
+
+ // Second Carousel init
+      $('.row-equal').slick({
+        slidesToShow: 5,
+        nextArrow: $(".nextButton"),
+        prevArrow: $(".prevButton"),
+        infinite: true,
+        doots:false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 780,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+
+        ]
+      });
+
+});
